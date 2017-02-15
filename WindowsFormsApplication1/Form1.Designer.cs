f4c787dae955156ba257e445c3e7981c9b3eafba
Knyazev Sergey <yaknyazsergei@yandex.ru>
2017-02-16 01:27:23 +0300

    update

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInfoAboutDisk = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.allDisks = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelInfoAboutDisk
+            // 
+            this.labelInfoAboutDisk.AutoSize = true;
+            this.labelInfoAboutDisk.Location = new System.Drawing.Point(197, 27);
+            this.labelInfoAboutDisk.Name = "labelInfoAboutDisk";
+            this.labelInfoAboutDisk.Size = new System.Drawing.Size(16, 13);
+            this.labelInfoAboutDisk.TabIndex = 1;
+            this.labelInfoAboutDisk.Text = "...";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(284, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Eject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // allDisks
+            // 
+            this.allDisks.FormattingEnabled = true;
+            this.allDisks.Location = new System.Drawing.Point(12, 27);
+            this.allDisks.Name = "allDisks";
+            this.allDisks.Size = new System.Drawing.Size(68, 95);
+            this.allDisks.TabIndex = 8;
+            this.allDisks.SelectedIndexChanged += new System.EventHandler(this.allDisks_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(101, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 170);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.allDisks);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelInfoAboutDisk);
+            this.Name = "Form1";
+            this.Text = "Lab1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelInfoAboutDisk;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox allDisks;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Management;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        const int OPEN_EXISTING = 3;
+        const uint GENERIC_READ = 0x80000000;
+        const uint GENERIC_WRITE = 0x40000000;
+        const uint IOCTL_STORAGE_EJECT_MEDIA = 0x2D4808;
+
+        [System.Runtime.InteropServices.DllImport("kernel32")]
+        private static extern int CloseHandle(IntPtr handle);
+
+        [System.Runtime.InteropServices.DllImport("kernel32")]
+        private static extern int DeviceIoControl
+            (IntPtr deviceHandle, uint ioControlCode,
+              IntPtr inBuffer, int inBufferSize,
+              IntPtr outBuffer, int outBufferSize,
+              ref int bytesReturned, IntPtr overlapped);
+
+        [System.Runtime.InteropServices.DllImport("kernel32")]
+        private static extern IntPtr CreateFile
+            (string filename, uint desiredAccess,
+              uint shareMode, IntPtr securityAttributes,
+              int creationDisposition, int flagsAndAttributes,
+              IntPtr templateFile);
+
+        List<string> kvp;
+
+        //Получение списка букв USB накопителей
+        private void UsbDiskList()
+        {
+            string diskName = string.Empty;
+            kvp = new List<string>();
+            //предварительно очищаем список
+            comboBox1.Items.Clear();
+
+            //Получение списка накопителей подключенных через интерфейс USB
+            foreach (System.Management.ManagementObject drive in
+                      new System.Management.ManagementObjectSearcher(
+                       "select * from Win32_DiskDrive where InterfaceType='USB'").Get())
+            {
+                //Получаем букву накопителя
+                foreach (System.Management.ManagementObject partition in
+                   new System.Management.ManagementObjectSearcher(
+                    "ASSOCIATORS OF {Win32_DiskDrive.DeviceID='" + drive["DeviceID"]
+                      + "'} WHERE AssocClass = Win32_DiskDriveToDiskPartition").Get())
+                {
+                    foreach (System.Management.ManagementObject disk in
+                       new System.Management.ManagementObjectSearcher(
+                        "ASSOCIATORS OF {Win32_DiskPartition.DeviceID='"
+                          + partition["DeviceID"]
+                          + "'} WHERE AssocClass = Win32_LogicalDiskToPartition").Get())
+                    {
+                        //Получение буквы устройства
+                        diskName = disk["Name"].ToString().Trim();
+                        comboBox1.Items.Add(diskName + " (" + drive["Model"] + ")");
+                        kvp.Add(diskName);
+                    }
+                }
+            }
+        }
+
+        //метод для извлечения USB накопителя
+        static void EjectDrive(string driveLetter)
+        {
+            string path = "\\\\.\\" + driveLetter;
+
+            IntPtr handle = CreateFile(path, GENERIC_READ | GENERIC_WRITE, 0,
+                IntPtr.Zero, OPEN_EXISTING, 0, IntPtr.Zero);
+
+            if ((long)handle == -1)
+            {
+                MessageBox.Show("Невозможно извлечь USB устройство!",
+           "Извлечение USB накопителей", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int dummy = 0;
+            DeviceIoControl(handle, IOCTL_STORAGE_EJECT_MEDIA, IntPtr.Zero, 0,
+                IntPtr.Zero, 0, ref dummy, IntPtr.Zero);
+            int returnValue = DeviceIoControl(handle, IOCTL_STORAGE_EJECT_MEDIA,
+                     IntPtr.Zero, 0, IntPtr.Zero, 0, ref dummy, IntPtr.Zero);
+            CloseHandle(handle);
+            MessageBox.Show("USB устройство, успешно извлечено!",
+         "Извлечение USB накопителей", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void LoadInfo()
+        {
+            //Загрузка букв USB накопителей при запуске программы
+            UsbDiskList();
+            //Выбор первого устройства в списке
+            if(comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+        }
+
+        static readonly string[] SizeSuffixes =
+                  { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        static string SizeSuffix(Int64 value, int decimalPlaces = 1)
+        {
+            if (value < 0) { return "-" + SizeSuffix(-value); }
+
+            int i = 0;
+            decimal dValue = (decimal)value;
+            while (Math.Round(dValue, decimalPlaces) >= 1000)
+            {
+                dValue /= 1024;
+                i++;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}", dValue, SizeSuffixes[i]);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadInfo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            
+            foreach (var drive in allDrives)
+            {
+                allDisks.Items.Insert(0, drive.Name);
+            }
+
+            allDisks.SetSelected(0, true);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Items.Count > 0)
+            {
+                allDisks.SetSelected(0, true);
+                allDisks.Items.Remove(String.Format("{0}:\\", comboBox1.SelectedItem.ToString()[0]));
+                EjectDrive(kvp[comboBox1.SelectedIndex]);
+            }
+            LoadInfo();
+        }
+
+        private void allDisks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string diskName = allDisks.SelectedItem.ToString();
+            
+            pictureBox1.Invalidate();
+            pictureBox1.Update();
+            pictureBox1.Refresh();
+
+            DriveInfo di = new DriveInfo(allDisks.SelectedItem.ToString());
+
+            long availableFreeSpace = di.AvailableFreeSpace;
+            string driveFormat = di.DriveFormat;
+            string name = di.Name;
+            long totalSize = di.TotalSize;
+
+
+            string  text = String.Format("name {0}\n", name);
+            text += String.Format("drive Format {0}\n", driveFormat);
+            text += String.Format("available FreeSpace {0}\n", SizeSuffix(availableFreeSpace));
+            text += String.Format("total Size {0}", SizeSuffix(totalSize));
+
+            labelInfoAboutDisk.Text = text;
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            float offset = 0F;
+            RectangleF rect;
+            StringFormat sf = new StringFormat() { LineAlignment = StringAlignment.Center, Alignment = StringAlignment.Center };
+            
+            DriveInfo di = new DriveInfo(allDisks.SelectedItem.ToString());
+
+                if (di.IsReady)
+                {
+                    rect = new RectangleF(offset, 0F, 90F, 90F);
+                    e.Graphics.FillEllipse(Brushes.Green, rect);
+
+                    float proc = (float)(di.TotalSize - di.TotalFreeSpace) / (float)di.TotalSize;
+
+                    e.Graphics.FillPie(Brushes.Gray,
+                        rect.X, rect.Y, rect.Width, rect.Height,
+                        180F, 360F * proc);
+
+                    offset += rect.Width + 10F;
+                
+            }
+        }
+       
+    }
+}
